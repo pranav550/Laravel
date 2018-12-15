7d45dc7f71846c44610774d619212ed88e265cde
pranav <pranavverma550@gmail.com>
2018-12-15 20:19:25 +0530

    relationship

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Model\Question;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -27,4 +27,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function question(){
+        return $this->hasMany(Question::class) 
+       }
 }
